0c59a0c76aa3b80affb34d60577260637f12ad5f
vineet <vineetkumarsingh341@gmail.com>
2024-05-13 15:04:03 +0530

    f

--- src/constrants/ProtectedRoute.js
+++ src/constrants/ProtectedRoute.js
@@ -1,10 +1,13 @@
 import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import Cookies from 'js-cookie';
 
 const ProtectedRoute = ({ Component }) => {
   const navigate = useNavigate();
   const user = useSelector(state => state.auth.user);
+const token= Cookies.get('rocket-token');
+console.log("token*************", token);
 
   console.log("protected", user);
   
