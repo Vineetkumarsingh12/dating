60f2b96b1d513db6a6a02d566ab1b2ecf2659f0b
vineet <vineetkumarsingh341@gmail.com>
2024-05-13 15:35:13 +0530

    f

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,15 +1,13 @@
 import jwt from "jsonwebtoken";
 
 
-import { redirect } from "react-router-dom";
-
 const isAuthenticated = async(req, res, next) => {
     try{
 
   const token = req.cookies["rocket-token"];
   console.log("*********",token);
   if (!token)
-    return redirect("https://dating-nine-rose.vercel.app/login");
+    return res.redirect("https://dating-nine-rose.vercel.app/login");
 
   const decodedData = jwt.verify(token, process.env.JWT_SECRET);
 
