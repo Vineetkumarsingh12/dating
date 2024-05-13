c2f44ce38c9ee379b9fcceacaa998735a9b3868a
vineet <vineetkumarsingh341@gmail.com>
2024-05-13 15:30:20 +0530

    f

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,6 +1,7 @@
 import jwt from "jsonwebtoken";
 
-import { User } from "../model/user.js";
+
+import { redirect } from "react-router-dom";
 
 const isAuthenticated = async(req, res, next) => {
     try{
@@ -8,7 +9,7 @@ const isAuthenticated = async(req, res, next) => {
   const token = req.cookies["rocket-token"];
   console.log("*********",token);
   if (!token)
-    return res.status(401).json({ success: false, message: "Unauthorized" });
+    return redirect("https://dating-nine-rose.vercel.app/login");
 
   const decodedData = jwt.verify(token, process.env.JWT_SECRET);
 
--- src/constrants/ProtectedRoute.js
+++ src/constrants/ProtectedRoute.js
@@ -1,27 +1,12 @@
 import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import Cookies from 'js-cookie';
-import  { useState } from 'react';
+
 
 
 const ProtectedRoute = ({ Component }) => {
   const navigate = useNavigate();
   const user = useSelector(state => state.auth.user);
-  const [rocketToken, setRocketToken] = useState("");
-
-  useEffect(() => {
-
-      const rocketTokenCookie = document.cookie
-          .split('; ')
-          .find(row => row.startsWith('rocket token='));
-console.log("rocketTokenCookie",rocketTokenCookie);
-      if (rocketTokenCookie) {
-          const tokenValue = rocketTokenCookie.split('=')[1];
-          setRocketToken(tokenValue);
-          console.log("value*****",tokenValue);
-      }
-  }, []);
 
   console.log("protected", user);
   
